497164d2ab3c920b011e48067cd78ae4ea7a6361
Viktor Balchikliev <balchikliev@gmail.com>
2023-07-07 09:53:10 +0300

    Add first batch of files

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Viktor Balchikliev and this is my final project for Skillo 08 - Automation course.");
+    }
+}
