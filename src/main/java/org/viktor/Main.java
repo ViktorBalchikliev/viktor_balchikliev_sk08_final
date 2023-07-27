376d23675833854575c914bf8338f8e35af8ad2c
Viktor Balchikliev <balchikliev@gmail.com>
2023-07-27 09:16:12 +0300

    Remove main.java

--- src/main/java/org/viktor/Main.java
+++
@@ -1,7 +0,0 @@
-package org.viktor;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, my name is Viktor Balchikliev and this is my final project for Skillo 08 - Automation course.");
-    }
-}
